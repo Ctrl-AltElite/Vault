9a61ef409afe62519b76ba0c8e72aa1fbf9be4f3
Axolotl <elefterescustefanadrian@gmail.com>
2023-06-09 13:50:41 +0300

    linux terminal installer

--- installers/linux.sh
+++ installers/linux.sh
@@ -1,1 +1,4 @@
-echo "Testing for README.md"
+echo "Installing latest Vault version!"
+
+wget "https://github.com/Ctrl-AltElite/Vault/releases/download/v0.1.0/Vault_0.1.0-1_amd64.deb"
+sudo apt install "./Vault_0.1.0-1_amd64.deb"
